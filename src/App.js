bec97b09207cc6506dbb1bd4bba6c10822aea8ab
Igor Stojadinovic <67150758+IgorStojadinovic@users.noreply.github.com>
2021-05-25 19:47:48 +0200

    Add files via upload
    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
+
+function App() {
+  return (
+    <Fragment>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/services" exact component={Services} />
+          <Route path="/sign-up" exact component={SignUp} />
+        </Switch>
+      </Router>
+    </Fragment>
+  );
+}
+
+export default App;
